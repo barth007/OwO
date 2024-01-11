fbfa6aa510cbca89be5458a2bb3f9ef32a16d5ac
Milemotigo <96849435+Milemotigo@users.noreply.github.com>
2024-01-11 22:45:59 +0100

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -23,9 +23,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('user_auth.urls')),
     path('', include('core.urls')),
+    path('account/', include('account.urls')),
     path('', include('blog.urls')),
     path('company/', include('company.urls')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
